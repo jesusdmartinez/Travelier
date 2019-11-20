1c99eb910f603532d05dc09b7b9d514203acbeb8
Jesus Martinez <jesusd.martinez@Jesuss-MacBook-Pro.local>
2019-11-20 09:33:11 -0800

    Html

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,9 @@
 from flask import current_app as app
 from flask import render_template, jsonify, flash, request
+
 from .hello import HelloApi
-from .users import NewUsersApi
-from .messages import ChatsApi
-from .groups import GroupsApi
-from .models import User, Messages
+from .models import User
+
 from . import db
 
 
