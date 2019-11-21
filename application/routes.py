37cf82edd5da12f0359c17550582bb376fefa764
Jesus Martinez <jesusd.martinez@Jesuss-MacBook-Pro.local>
2019-11-20 16:47:51 -0800

    Routes and Users Files Updated

--- application/routes.py
+++ application/routes.py
@@ -2,12 +2,13 @@ from flask import current_app as app
 from flask import render_template, jsonify, flash, request
 
 from .hello import HelloApi
+from .users import NewUsersApi
 from .models import User
 
 from . import db
 
-
 app.register_blueprint(HelloApi, url_prefix='/hello')
+app.register_blueprint(NewUsersApi, url_prefix='/')
 
 
 @app.route('/')
