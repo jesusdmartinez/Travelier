3dad3931f6fe71aa0df1469fef3339a7361d76bb
Jesus Martinez <jesusd.martinez@gmail.com>
2019-11-22 12:02:21 -0800

    Html, js, css, update

---
+++ application/static/js/main.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+				$('.btn1').on('click', function(){
+				$('.btn1').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn2').on('click', function(){
+				$('.btn2').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn3').on('click', function(){
+				$('.btn3').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn4').on('click', function(){
+				$('.btn4').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn5').on('click', function(){
+				$('.btn5').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn6').on('click', function(){
+				$('.btn6').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn7').on('click', function(){
+				$('.btn7').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.btn8').on('click', function(){
+				$('.btn8').removeClass('active')
+				$(this).addClass('active')
+				});
+				$('.next1').on('click', function(){
+				$('.travel-bg').removeClass('travel1');
+				$('.travel-bg').addClass('travel2')
+				});
+				$('.prev2').on('click', function(){
+				$('.travel-bg').removeClass('travel2');
+				$('.travel-bg').addClass('travel1')
+				});
+				$('.next2').on('click', function(){
+				$('.travel-bg').removeClass('travel2');
+				$('.travel-bg').addClass('travel3')
+				});
+				$('.prev3').on('click', function(){
+				$('.travel-bg').removeClass('travel3');
+				$('.travel-bg').addClass('travel2')
+				});
+			    $('.next3').on('click', function(){
+				$('.travel-bg').removeClass('travel3');
+				$('.travel-bg').addClass('travel4')
+				});
+				$('.prev4').on('click', function(){
+				$('.travel-bg').removeClass('travel4');
+				$('.travel-bg').addClass('travel3')
+				});
+				  $('.next4').on('click', function(){
+				$('.travel-bg').removeClass('travel4');
+				$('.travel-bg').addClass('travel5')
+				});
+				$('.prev5').on('click', function(){
+				$('.travel-bg').removeClass('travel5');
+				$('.travel-bg').addClass('travel4')
+				});
+				  $('.next5').on('click', function(){
+				$('.travel-bg').removeClass('travel5');
+				$('.travel-bg').addClass('travel6')
+				});
+				$('.prev6').on('click', function(){
+				$('.travel-bg').removeClass('travel6');
+				$('.travel-bg').addClass('travel5')
+				});
+					  $('.next6').on('click', function(){
+				$('.travel-bg').removeClass('travel6');
+				$('.travel-bg').addClass('travel7')
+				});
+				$('.prev7').on('click', function(){
+				$('.travel-bg').removeClass('travel7');
+				$('.travel-bg').addClass('travel6')
+				});
+					  $('.next7').on('click', function(){
+				$('.travel-bg').removeClass('travel7');
+				$('.travel-bg').addClass('travel8')
+				});
+				$('.prev8').on('click', function(){
+				$('.travel-bg').removeClass('travel8');
+				$('.travel-bg').addClass('travel7')
+				});
+		});
