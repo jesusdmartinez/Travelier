cda5c2629a84184ed16fc29891f3fa4722be058d
Jesus Martinez <jesusd.martinez@gmail.com>
2019-12-11 18:58:57 -0800

    Send email

--- application/send_email.py
+++ application/send_email.py
@@ -11,27 +11,23 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
 
     subject = "Grab dinner this weekend"
     body = "How about dinner at 6pm this saturday?"
-
-
     msg = f'Subject: {subject}\n\n{body}'
 
-    smpt.sendmail(secrets.EMAIL_ADDRESS, secrets.EMAIL_ADDRESS, msg)
+    smtp.sendmail(secrets.EMAIL_ADDRESS, secrets.EMAIL_ADDRESS, msg)
 
 
 
 
 # def send_email(subject, msg):
-#     try:
-#         server = smtplib.SMTP('smtp.gmail.com', 587)
-#         server.ehlo()
-#         server.starttls()
-#         server.login(secrets.EMAIL_ADDRESS, secrets.PASSWORD)
-#         message = 'Subject: {}\n\n{}'.format(subject, msg)
-#         server.sendmail(secrets.EMAIL_ADDRESS, secrets.EMAIL_ADDRESS, message)
-#         server.quit()
-#         print("Success, email sent")
-#     except:
-#         print("Email failed to send")
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.ehlo()
+#     server.starttls()
+#     server.login('jesus.martinez@thetravelier.com', 'alwaystravel')
+#     message = 'Subject: {}\n\n{}'.format(subject, msg)
+#     server.sendmail('jesus.martinez@thetravelier.com', 'jesus.martinez@thetravelier.com', message)
+#     server.quit()
+#     print("Success, email sent")
+#
 #
 # Subject = "Test"
 # msg = "Hello"
